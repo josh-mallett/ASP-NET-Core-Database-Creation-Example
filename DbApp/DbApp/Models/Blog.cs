018d587e99008dbdd59ab110e1f13dc169c61e07
Josh Mallett <playgroundinfinity@gmail.com>
2017-07-16 13:21:46 -0500

    Add files via upload

---
+++ DbApp/DbApp/Models/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DbApp.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string url { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ DbApp/DbApp/Models/BloggingContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DbApp.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options) : base (options)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
